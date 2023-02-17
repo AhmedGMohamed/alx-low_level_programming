e7929378e76121d2cb01139f1a756a5585901e2e
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-17 04:47:52 +0200

    0x00.101 C Program that outputs to stderr without fprintf or fputs

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Outputs the required text to stderr without fprintf or fputs
+ *
+ * Return: 1 if the code runs successfully
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
