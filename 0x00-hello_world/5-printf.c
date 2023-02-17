a4541dec43fd32390811b2cdc741d2ac742b97a2
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-17 04:11:30 +0200

    0x00.5 C Program that outputs required text using printf to stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Outputs text to stdout using printf
+ *
+ * Return: 0 if the code executed successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
