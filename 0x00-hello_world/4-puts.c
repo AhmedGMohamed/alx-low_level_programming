ad590e78bd3f0798801ae2d1121f48938d16c305
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-17 03:51:55 +0200

    0x00.4 C program that outputs required text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* Main function printing some string text to stdout */
+int main(void)
+{
+	puts("\"Programming is like building"
+		" a multilingual puzzle\n");
+	return (0);
+}
