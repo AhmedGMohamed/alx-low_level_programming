822a84b4cfd4208cc7591fe1f42bd7808ec982ab
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-17 05:36:43 +0200

    0x01.0 C program that checks for a random numbers sign

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Checks for the sign of a random number
+ *
+ * Return: 0 if the code executes successfully
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
