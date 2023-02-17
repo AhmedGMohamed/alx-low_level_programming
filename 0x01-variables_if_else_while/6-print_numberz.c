2a16d2ca121435a5289ec637109dd4d92cc76cbc
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-17 16:45:24 +0200

    0x01.6 Updated Documentation

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print all single digit numbers from 0 to 10
+ * main - print all single digit numbers from 0 to 10 using putchar
  *
  * Return: 0 if the code executes successfully
  */
