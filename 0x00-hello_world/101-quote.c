2a35a7581570deb3f29cf5fd09d7ece51d850f56
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-17 05:00:39 +0200

    0x00.101 C Program that outputs to stderr without fprintf or fputs, version 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19",
-		strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"),
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"),
 		1, stderr);
 	return (1);
 }
