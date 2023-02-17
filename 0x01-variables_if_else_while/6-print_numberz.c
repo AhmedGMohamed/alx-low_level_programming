e13def385c8eac77217b223b850438426130ba7b
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-17 16:41:07 +0200

    0x01.6 C Program that prints all single digit numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers from 0 to 10
+ *
+ * Return: 0 if the code executes successfully
+ */
+int main(void)
+{
+	char n = 0x2F, nl = 0x0A;
+
+	while (n++ < 0x39)
+	{
+		putchar(n);
+	}
+	putchar(nl);
+	return (0);
+}
