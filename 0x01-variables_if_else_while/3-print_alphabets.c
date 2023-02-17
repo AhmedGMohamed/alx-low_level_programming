76614f9959e8e1df017138181136c43033e57051
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-17 16:15:54 +0200

    0x01.3 C Program to print all alphabet characters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all lowercase alphabet letters
+ *
+ * Return: 0 if the code executes successfully
+ */
+int main(void)
+{
+	char ch = 0x60, newline = 0x0A;
+
+	while (ch++ != 0x7A)
+	{
+		putchar(ch);
+	}
+
+	ch = 0x40;
+
+	while (ch++ != 0x5A)
+	{
+		putchar(ch);
+	}
+	putchar(newline);
+	return (0);
+}
