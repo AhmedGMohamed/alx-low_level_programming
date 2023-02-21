fe67a9888778b3fe2861e93db45d52a8be8b5cdf
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-21 19:27:46 +0200

    0x02.0 Program to output "_putchar" to stdout using _putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Prints "_putchar" to stdout using _putchar.c file
+ *
+ * Return: 0 if the code executes successfully
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef _PUTCHAR
+#define _PUTCHAR
+/**
+ * _putchar - Outputs a character to stdout
+ * @c: The character to output to stdout
+ *
+ * Return: On success 1
+ * On error, -1 is returned and errno is set appropriately
+ */
+int _putchar(char c);
+
+#endif
