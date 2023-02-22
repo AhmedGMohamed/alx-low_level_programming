2dca1d39691ecb93559f83f0a79b1c824252678d
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-22 03:30:36 +0200

    0x02.7 Fixed negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,22 @@
 #include "main.h"
+
+/**
+ * _abs - computes the absolute value of a number
+ * @n: the number to compute
+ *
+ * Return: n, the absolute value
+ */
+
+int _abs(int n)
+
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
+
 /**
  * print_last_digit - returns the last digit in the given number
  * @n: the number to get the digit from
@@ -10,6 +28,7 @@ int print_last_digit(int n)
 {
 	int dig;
 
+	n = _abs(n);
 	dig = n % 10;
 	_putchar(dig+'0');
 	return (dig);
