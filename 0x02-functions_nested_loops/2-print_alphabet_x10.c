b691d9fe4ad94183af73e289eb09906d5604bfc1
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-22 02:30:16 +0200

    0x02.2 C Program that prints lowercase alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a new line
+ */
+
+void print_alphabet(void)
+
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		char letter = 97;
+
+		while (letter != 123)
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
