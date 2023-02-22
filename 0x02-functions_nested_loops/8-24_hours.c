4937a8223fe3a2a6923d2518831e729893e0f82e
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-22 03:55:07 +0200

    0x02.8 Fixed incorrect output bug

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,10 +14,10 @@ void jack_bauer(void)
 
 		for (mins = 0; mins < 60; mins++)
 		{
-			_putchar(hours % 100 + '0');
+			_putchar(hours / 10 + '0');
 			_putchar(hours % 10 + '0');
 			_putchar(':');
-			_putchar(mins % 100 + '0');
+			_putchar(mins / 10 + '0');
 			_putchar(mins % 10 + '0');
 			_putchar('\n');
 		}
