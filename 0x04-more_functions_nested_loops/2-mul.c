eaf9408dce25757019cee8c70194bb7905341c0f
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-24 01:28:06 +0200

    0x04.1 Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digits
+ * @a: the first nubmer to mulitply
+ * @b: the second number to multiply
+ *
+ * Return: prod, the product of a * b
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
