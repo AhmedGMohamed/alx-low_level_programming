31ab9e724f35226749994d988b4b1c7df663a452
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-24 01:59:23 +0200

    0x04.7 Print a diagonal line to the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints a diagonal line
+ * @n: the height of the line to print
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
