9fb8a6fe8ebd0915b22048eb0cc92e2f593ffc79
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-27 08:23:51 +0200

    0x05.5 Reverse an array of character (string)

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a given array of characters (string)
+ * @s: the array of characters (string) to reverse
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		_putchar(s[len]);
+		len++;
+	}
+	len--;
+	int, limit = len / 2;
+
+	for (i = 0; i < limit; i++, len--)
+	{
+		char temp;
+
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+	}
+}
