b8abaf820d1e07c9419a5198ba98618ae45bc390
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-27 07:23:34 +0200

    0x05.3 Print array of characters (string) to stdout reversed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - outputs an array of characters (string) to stdout reversed
+ * @s: the array of characters (string) to output
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
