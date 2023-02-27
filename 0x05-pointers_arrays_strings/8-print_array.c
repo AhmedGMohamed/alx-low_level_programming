44f85eece61c0748f226a766bd233ec75221dc3c
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-02-27 08:54:03 +0200

    0x05.8 Prints N elements of array of integers to stdout

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Outputs a number of elements from array to stdout
+ * @a: the array of integers to output
+ * @n: the number of array elements to output
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	_putchar('\n');
+}
