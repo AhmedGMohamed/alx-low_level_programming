6d553fd21cfe80e53512e4f4ce9e6a4cce8f56ea
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-01 07:17:58 +0200

    0x06.7 Simplified string encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into simplified 1337
+ * @s: a pointer the string
+ *
+ * Return: s - the encoded string
+ */
+
+char *leet(char *s)
+{
+	char enc[17] = "aA4eE3oO0tT7lL7";
+	int len = 0, i;
+
+	while (s[len] != '\0')
+	{
+		for (i = 0; i < 15; i = i + 3)
+		{
+			if (s[len] == enc[i] || s[len] == enc[i + 1])
+			{
+				s[len] = enc[i + 2];
+			}
+		}
+		len++;
+	}
+	return (s);
+}
