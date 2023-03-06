6379b4da5dfb2cd68f156cda594aaa62ce435967
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-06 14:45:36 -0800

    0x07.0 Fill first n bytes of buffer with constant char

---
+++ 0x07-pointers_array_string/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first 'n' bytes with a given constant byte
+ * @s: the buffer to fill
+ * @b: the constant byte to fill the buffer with
+ * @n: the number of bytes to replace
+ *
+ * Return: s - the modified buffer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return s;
+}
