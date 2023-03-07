eaff0c4f360023d8e3630dc17dff5489bcfc1704
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-06 18:26:03 -0800

    0x07.100 Set Pointer of string to a string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - sets the string 's' to the content of 'to'
+ * @s: the pointer to string to change
+ * @to: the string to copy into 's'
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
