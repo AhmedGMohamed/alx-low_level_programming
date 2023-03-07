3dfad26075658d484c8d6104d4fc524af75458ed
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-06 16:44:31 -0800

    0x07.3 Finds the number of bytes in "s" included in "accept"

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 int _strlen(char *);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
