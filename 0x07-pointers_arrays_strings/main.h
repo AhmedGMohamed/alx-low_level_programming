aeb4ade0e43d1aa020456e04d9334f838d34e317
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-06 17:40:26 -0800

    0x07.6 Print a chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
@@ -8,5 +9,6 @@ int _strlen(char *);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
+void print_chessboard(char (*)[8]);
 
 #endif
