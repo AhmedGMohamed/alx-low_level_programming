9f0220ff6ee1be9972942b05722fd30b30976236
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-10 14:10:48 -0800

    0x0A.2 Multiply two numbers using supplied arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the product of two number
+ * @argc: the number of arguments
+ * @argv: array of argument strings
+ *
+ * Return: 0 if the code executes successfully
+ *	   1 if not enough arguments are supplied
+ */
+int main(int argc, char *argv[])
+{
+	int prod;
+
+	if (argc < 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	prod = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", prod);
+	return (0);
+}
