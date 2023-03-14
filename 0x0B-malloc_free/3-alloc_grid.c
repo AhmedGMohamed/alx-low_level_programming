6fef8746f12530022da9f82cacd8c8797ce8d17b
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-14 14:54:14 -0700

    0x0B.3 Styling edit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 		row = malloc(sizeof(int) * width);
 		if (row == NULL)
 		{
-			for (j = i; j >=0; j--)
+			for (j = i; j >= 0; j--)
 			{
 				free(arr[j]);
 			}
