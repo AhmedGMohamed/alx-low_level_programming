4a268990ff947ea986e8343f59b8a20895899bc8
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-14 11:19:11 -0700

    0x0B.0 Fixed Segmentation error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,11 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	if (size == 0)
-	{
-		return (NULL);
-	}
 	ptr = malloc(sizeof(c) * size);
+	if (ptr == NULL)
+	{
+		return (ptr);
+	}
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
