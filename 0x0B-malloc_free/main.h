8dbf83b6626b10ffa95bf7f5ef51144335ae2edc
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-14 16:07:26 -0700

    0x0B.100 Concatenate all program arguments into one string

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char);
 int _strlen(char *);
 char *create_array(unsigned int, char);
@@ -8,4 +7,5 @@ char *_strdup(char *);
 char *str_concat(char *, char *);
 int **alloc_grid(int, int);
 void free_grid(int **, int);
+char *argstostr(int, char **);
 #endif
