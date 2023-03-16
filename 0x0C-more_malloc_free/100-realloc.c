3bd1ccd057cf7708f58b1e36b109b9a8b5a192b3
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-16 14:17:41 -0700

    0x0C.100 Changed order of execution in the first checks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,10 +13,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	char *old = ptr, *new;
 
-	if (new_size == old_size)
-		return (ptr);
 	if (ptr == NULL)
 		return (malloc(new_size));
+	if (new_size == old_size)
+		return (ptr);
 	if (new_size == 0)
 		free(ptr);
 	new = malloc(new_size);
