b2cf9cf80b1426eff71d0435d4a03fa308335667
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-15 20:01:44 -0700

    0x0B.101 Split a string into an array of words

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,4 +8,7 @@ char *str_concat(char *, char *);
 int **alloc_grid(int, int);
 void free_grid(int **, int);
 char *argstostr(int, char **);
+char **strtow(char *);
+int word_cnt(char *);
+int word_len(char *);
 #endif
