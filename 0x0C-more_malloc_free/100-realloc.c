5ba1a448c78a5a040f572e153160719050c3689d
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-16 14:24:04 -0700

    0x0C.100 Added missing return and free statements

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,10 +15,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 		return (malloc(new_size));
-	if (new_size == old_size)
-		return (ptr);
 	if (new_size == 0)
+	{
 		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		free(ptr);
+		return (ptr);
+	}
 	new = malloc(new_size);
 	if (new == NULL)
 		return (NULL);
