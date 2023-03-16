0170423bfc862a6a99e00fea68227c5cb1291d37
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-16 10:44:23 -0700

    0x0C.1 Fixed n smaller than s2 length case

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,7 +43,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len1; i++)
 		s3[i] = s1[i];
 	offset = i;
-	for (i = 0; i <= n && i <= len2; i++)
+	for (i = 0; i < n && i < len2; i++)
 		s3[offset + i] = s2[i];
+	se[offset + i] = '\0';
 	return (s3);
 }
