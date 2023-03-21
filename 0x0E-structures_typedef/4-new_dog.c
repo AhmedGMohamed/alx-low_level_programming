0f2c30423b94692beaccac5840af602f1b5762b4
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-20 19:24:30 -0700

    0x0E.4 Removed redundant code & added null case handling\

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,26 +28,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *new_name, *new_owner;
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
+	if (new_dog == NULL || name == NULL || owner == NULL)
 		return (NULL);
 	len = _strlen(name);
 	new_name = malloc(sizeof(char) * len);
 	if (new_name == NULL)
 		return (NULL);
-/*	for (i = 0; i < len; i++)
-		new_name[i] = name[i];
-	new_name[i] = '\0';
-	new_dog->name = new_name;
-*/	new_dog->name = name;
+	new_dog->name = name;
 	new_dog->age = age;
 	len = _strlen(owner);
 	new_owner = malloc(sizeof(char) * len);
 	if (new_owner == NULL)
 		return (NULL);
-/*	for (i = 0; i < len; i++)
-		new_owner[i] = owner[i];
-	new_owner[i] = '\0';
-	new_dog->owner = new_owner;
-*/	new_dog->owner = owner;
+	new_dog->owner = owner;
 	return (new_dog);
 }
