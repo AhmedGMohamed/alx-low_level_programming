97ae012b2976e341041e3efb61fbf5aed81ed810
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-20 19:13:43 -0700

    0x0E.4 Dynamically allocate struct dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * _strlen - find the string's length
+ * @s: the string to check
+ *
+ * Return: len - the string's length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * new_dog - dynamically allocates a dog struct
+ * @name: string containing the name of the dog
+ * @age: the age of the dog
+ * @owner: string containing the name of the owner
+ *
+ * Return: a pointer to dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int len, i;
+	char *new_name, *new_owner;
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+	len = _strlen(name);
+	new_name = malloc(sizeof(char) * len);
+	if (new_name == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		new_name[i] = name[i];
+	new_name[i] = '\0';
+	new_dog->name = new_name;
+	new_dog->age = age;
+	len = _strlen(owner);
+	new_owner = malloc(sizeof(char) * len);
+	if (new_owner == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		new_owner[i] = owner[i];
+	new_owner[i] = '\0';
+	new_dog->owner = new_owner;
+	return (new_dog);
+}
