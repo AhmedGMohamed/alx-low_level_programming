4df583d9b8c85bae69529e81101451c873a20f0d
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-22 15:32:48 -0700

    0x0F.1 Perform operation on array elements using function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stdlib.h>
+#include <stddef.h>
 void print_name(char *, void (*)(char *));
+void array_iterator(int *, size_t, void (*)(int));
 #endif
