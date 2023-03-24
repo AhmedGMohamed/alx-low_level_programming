36e91acdee177a89accc1a9ab80ab30b18f0754e
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-23 19:10:27 -0700

    0x10.3 Print each arugument in its specified format

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,9 @@ int _putchar(char);
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *, const unsigned int , ...);
 void print_strings(const char *, const unsigned int , ...);
+void print_c(char);
+void print_i(int);
+void print_f(float);
+void print_s(char *);
+void print_all(const char * const, ...);
 #endif
