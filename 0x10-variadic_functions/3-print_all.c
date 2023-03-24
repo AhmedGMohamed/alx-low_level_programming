1ac3ac7cd66f77547b4c9101ce5dd442c1a7d2ec
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-23 19:33:54 -0700

    0x10 Refactored code & removed extra if case

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -45,41 +45,41 @@ void print_s(char *s)
 void print_all(const char * const format, ...)
 {
 	int len = 0, i = 0, prnt = 0, prntd = 0, ent = 0;
-	char c;
 	va_list args;
 
-	while (format != NULL && format[len] != '\0')
+	while (format != NULL && format[i] != '\0')
 	{
-		c = format[len], len++;
-		if (c == 'c' || c == 'i' || c == 'f' || c == 's')
-			prnt++;
-	}
-	va_start(args, format);
-	while (i < len && prntd < prnt)
-	{
-		ent = 0;
-/*		switch (format[i])
+		switch (format[i++])
 		{
 		case 'c':
-			print_c(va_arg(args, int));
-			prntd++, ent++;
-			break;
 		case 'i':
-			print_i(va_arg(args, int));
-			prntd++, ent++;
-			break;
 		case 'f':
-			print_f(va_arg(args, double));
-			prntd++, ent++;
-			break;
 		case 's':
-			print_s(va_arg(args, char *));
-			prntd++, ent++;
+			prnt++;
 			break;
 		}
-*/		if (ent && prntd != prnt)
+	}
+	va_start(args, format);
+	while (format != NULL && format[len] != '\0')
+	{
+		ent = 0;
+		switch (format[len++])
+		{
+		case 'c':
+			print_c(va_arg(args, int)), prntd++, ent++;
+			break;
+		case 'i':
+			print_i(va_arg(args, int)), prntd++, ent++;
+			break;
+		case 'f':
+			print_f(va_arg(args, double)), prntd++, ent++;
+			break;
+		case 's':
+			print_s(va_arg(args, char *)), prntd++, ent++;
+			break;
+		}
+		if (ent && prntd != prnt)
 			printf(", ");
-		i++;
 	}
 	printf("\n");
 }
