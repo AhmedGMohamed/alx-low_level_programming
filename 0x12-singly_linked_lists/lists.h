3d827a27f41d090d60f398e09129227757363f65
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-30 06:18:23 -0700

    0x12.0 Print all data of a linked list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * list_t - typedef for list type
+ */
+typedef struct list list_t;
+/**
+ * struct list - A node for a linked list
+ * @str: a string
+ * @len: the length of the string
+ * @next: pointer to the next element in the linked list
+ */
+struct list
+{
+	char *str;
+	int len;
+	list_t *next;
+};
+int _putchar(char);
+size_t print_list(list_t *h);
+#endif
