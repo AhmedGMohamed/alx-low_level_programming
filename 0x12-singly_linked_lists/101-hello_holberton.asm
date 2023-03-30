4c44d6d8535edca5fa81b5d3d369544816e3ffb8
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-03-30 08:57:10 -0700

    0x12.101 print "Hello, Holberton" compiled using nasm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,45 +1,26 @@
-	.file	"101-hello_holberton.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
+section .text                   ;section declaration
+
+;we must export the entry point to the ELF linker or
+global  main              ;loader. They conventionally recognize _start as their
+;entry point. Use ld -e foo to override the default.
+
 main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
+
+;write our string to stdout
+
+mov     edx,len             ;third argument: message length
+mov     ecx,msg             ;second argument: pointer to message to write
+mov     ebx,1               ;first argument: file handle (stdout)
+mov     eax,4               ;system call number (sys_write)
+int     0x80                ;call kernel
+
+;and exit
+
+mov     ebx,0               ;first syscall argument: exit code
+mov     eax,1               ;system call number (sys_exit)
+int     0x80                ;call kernel
+
+section .data                   ;section declaration
+
+msg db      "Hello, Holberton",0xa ;our dear string
+len equ     $ - msg             ;length of our dear string
