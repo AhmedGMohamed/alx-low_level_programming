b20356a2275a6875e914ee03c12495dd071b78b2
Ahmed Gamal <gamal_aharam3@yahoo.com>
2023-04-04 09:55:27 -0700

    0x13.1 Return the number of elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: pointer to the start of the list
+ *
+ * Return: len - the length/number of elements of the list
+ */
+size_t list_len(listint_t *h)
+{
+	size_t len = 0;
+	listint_t *current = h;
+
+	while (current != NULL)
+	{
+		len++;
+		current = current->next;
+	}
+	return (len);
+}
