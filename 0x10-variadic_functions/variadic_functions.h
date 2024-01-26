d2863c33d7af800dffd47bd15ac6cea6156d275f
Ahmed Gamal <ahmedg.mohamed07@gmail.com>
2024-01-26 02:32:18 +0200

    0x10 Header file styling fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,8 +4,8 @@
 #include <stdarg.h>
 int _putchar(char);
 int sum_them_all(const unsigned int, ...);
-void print_numbers(const char *, const unsigned int , ...);
-void print_strings(const char *, const unsigned int , ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
 void print_c(char);
 void print_i(int);
 void print_f(float);
