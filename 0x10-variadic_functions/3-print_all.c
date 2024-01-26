546f00867d291129bc362a946cff62cc064080fc
Ahmed Gamal <ahmedg.mohamed07@gmail.com>
2024-01-26 02:18:49 +0200

    0x10 Vardiac Functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,85 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_c - prints a character
+ * @c: the character to print
+ */
+void print_c(char c)
+{
+	printf("%c", c);
+}
+/**
+ * print_i - prints an integer
+ * @i: the integer to print
+ */
+void print_i(int i)
+{
+	printf("%d", i);
+}
+/**
+ * print_f - prints a float
+ * @f: the float to print
+ */
+void print_f(float f)
+{
+	printf("%f", f);
+}
+/**
+ * print_s - prints a string
+ * @s: the string to print
+ */
+void print_s(char *s)
+{
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
+/**
+ * print_all - prints arguments depending on the format given
+ * @format: the format of each argument
+ * @...: the arguments to print
+ */
+void print_all(const char * const format, ...)
+{
+	int len = 0, i = 0, prnt = 0, prntd = 0, ent = 0;
+	va_list args;
+
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i++])
+		{
+		case 'c':
+		case 'i':
+		case 'f':
+		case 's':
+			prnt++;
+			break;
+		}
+	}
+	va_start(args, format);
+	while (format != NULL && format[len] != '\0')
+	{
+		ent = 0;
+		switch (format[len++])
+		{
+		case 'c':
+			print_c(va_arg(args, int)), prntd++, ent++;
+			break;
+		case 'i':
+			print_i(va_arg(args, int)), prntd++, ent++;
+			break;
+		case 'f':
+			print_f(va_arg(args, double)), prntd++, ent++;
+			break;
+		case 's':
+			print_s(va_arg(args, char *)), prntd++, ent++;
+			break;
+		}
+		if (ent && prntd != prnt)
+			printf(", ");
+	}
+	printf("\n");
+}
